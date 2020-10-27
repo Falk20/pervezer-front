895c2dd290e428995d07c7ffe8106684062d0893
Falk20 <petruk0908@gmail.com>
2020-10-27 17:37:09 +0300

    add firebase

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzFwWDuLmHjieZ3M5kyGtMPCQ6zepBLMQ",
+  authDomain: "pervezer-front.firebaseapp.com",
+  databaseURL: "https://pervezer-front.firebaseio.com",
+  projectId: "pervezer-front",
+  storageBucket: "pervezer-front.appspot.com",
+  messagingSenderId: "987643677881",
+  appId: "1:987643677881:web:c377f4265b55cb8efcdf68",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
