e9ad5c6eac8b4fd3d808605e111dd4b6f814c26b
Falk20 <petruk0908@gmail.com>
2020-11-03 22:52:13 +0300

    remove logo

--- src/views/Client.vue
+++ src/views/Client.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="d-flex flex-column fill-height align-center">
     <h1>Клиенты</h1>
-    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="d-flex flex-column fill-height align-center">
     <h1>Главная</h1>
-    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "Home"
 };
 </script>
