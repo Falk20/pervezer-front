c91e07b29087910f555f2fd1e14cf0d2b0f8e669
hnconnoronaseld <alex-hardpw@mail.ru>
2020-11-06 02:22:08 +0300

    v1.4
    fgd

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,8 @@
 const serverIP = "http://94.242.58.230:81";
 
 export const AUTH = `${serverIP}/api/Auth/authorization`;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
+
 export const GET_CLIENTS = `${serverIP}/api/Client/get-clients`;
 export const GET_PROFILES = `${serverIP}/api/Client/get-profiles`;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
-export const CLIENTS = `${serverIP}/api/Client/get-clients`;
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
+
