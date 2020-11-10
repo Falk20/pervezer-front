001d6f61769b2ac918db3a000d9fabba82c0ea6b
Falk20 <petruk0908@gmail.com>
2020-11-10 23:34:47 +0300

    cteate requisite components

---
+++ src/components/ClientDetail/Requisites/CreateRequisiteForm.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Новый адрес
+    </v-card-title>
+
+    <v-form
+      ref="form"
+      @submit.prevent="saveNewIp"
+      class="pa-1"
+      v-model="isValid"
+    >
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="address"
+              label="Адрес"
+              :rules="[rules.required]"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn
+              width="100%"
+              type="submit"
+              color="primary"
+              :loading="sending"
+            >
+              Создать
+            </v-btn>
+          </v-col>
+          <v-col>
+            <v-btn width="100%" type="button" @click="close">Отменить</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-alert
+        v-model="isErr"
+        dense
+        outlined
+        type="error"
+        dismissible
+        transition="scale-transition"
+      >
+        Не удалось добавить адрес
+      </v-alert>
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+import Axios from "axios";
+import { CREATE_ADDRESS } from "@/api";
+
+export default {
+  name: "client-detail-requisite-create-form",
+
+  data() {
+    return {
+      isValid: true,
+      address: "",
+      sending: false,
+      isErr: false,
+      rules: {
+        required: v => !!v || "Обязательное поле"
+      }
+    };
+  },
+
+  methods: {
+    async saveNewIp() {
+      try {
+        this.$refs.form.validate();
+
+        if (this.isValid) {
+          this.sending = true;
+
+          const { status } = await Axios.post(CREATE_ADDRESS, {
+            clientId: this.$route.params.clientID,
+            address: this.address
+          });
+
+          if (status === 200) {
+            this.$emit("create-address");
+
+            this.close();
+          }
+        }
+      } catch {
+        this.isErr = true;
+      } finally {
+        this.sending = false;
+      }
+    },
+    close() {
+      this.$emit("close");
+      this.address = "";
+    }
+  }
+};
+</script>
---
+++ src/components/ClientDetail/Requisites/index.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="d-flex flex-column align-center">
+    <p v-if="isLoad">Load...</p>
+    <p v-else-if="isErr">Error</p>
+    <v-card v-else width="400">
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          prepend-icon="mdi-magnify"
+          label="Поиск"
+          single-line
+          hide-details
+          clearable
+        />
+      </v-card-title>
+
+      <v-data-table
+        :headers="tableHeaders"
+        :items="addresses"
+        item-key="id"
+        :search="search"
+      >
+        <template v-slot:no-data>Нет адресов</template>
+        <template v-slot:no-results>Нет таких адресов</template>
+
+        <template v-slot:item.id="{ item }">
+          <v-btn color="red" dark @click="removeAddess(item)">
+            <v-icon dark>
+              mdi-delete
+            </v-icon>
+          </v-btn>
+        </template>
+      </v-data-table>
+
+      <template>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on, attrs }">
+            <div class="text-center pt-2">
+              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                Добавить адрес
+              </v-btn>
+            </div>
+          </template>
+          <CreateRequisiteForm
+            @create-address="addNewAddress"
+            @close="dialog = false"
+          />
+        </v-dialog>
+      </template>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { GET_CLIENT_ADDRESSES, REMOVE_ADDRESS } from "@/api";
+import Axios from "axios";
+
+import CreateRequisiteForm from "./CreateRequisiteForm";
+
+export default {
+  name: "client-details-requisites",
+
+  components: {
+    CreateRequisiteForm
+  },
+
+  data() {
+    return {
+      search: "",
+      isLoad: true,
+      isErr: false,
+      dialog: false,
+      addresses: null,
+      tableHeaders: [
+        {
+          text: "Адрес",
+          value: "address",
+          width: "100%"
+        },
+        {
+          text: "",
+          value: "id",
+          sortable: false
+        }
+      ]
+    };
+  },
+
+  methods: {
+    async getAddresses() {
+      try {
+        const { data: addresses } = await Axios.get(GET_CLIENT_ADDRESSES, {
+          params: {
+            id: this.$route.params.clientID
+          }
+        });
+
+        this.addresses = addresses;
+      } catch {
+        this.isErr = true;
+      } finally {
+        this.isLoad = false;
+      }
+    },
+
+    formatDate(date) {
+      return new Date(date).toLocaleDateString("ru", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric"
+      });
+    },
+
+    async removeAddess(item) {
+      try {
+        const { status } = await Axios.post(REMOVE_ADDRESS, {
+          addressId: item.id
+        });
+
+        if (status === 200) {
+          const itemIndex = this.addresses.indexOf(item);
+
+          this.addresses.splice(itemIndex, 1);
+        } else {
+          throw new Error();
+        }
+      } catch {
+        alert("Не удалось удалить ip");
+      }
+    },
+
+    addNewAddress() {
+      this.getAddresses();
+    }
+  },
+
+  created() {
+    this.getAddresses();
+  }
+};
+</script>
